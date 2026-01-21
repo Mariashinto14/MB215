1e9e9db381bec6bc83fed411156c3349950da49c
Your Name <2mariashinto14@gmail.com>
2026-01-20 20:32:41 -0500

    Lab 2

--- Lab2/Lab2_Activity6.py
+++ Lab2/Lab2_Activity6.py
@@ -1,25 +1,27 @@
 # Get two numbers
 num1 = float(input("Enter value of a: "))
 num2 = float(input("Enter value of b: "))
-# Calculate sum, difference, product, and quotient
-sum  = num1 + num2
-difference  = num1 - num2
+
+# Calculations
+addition = num1 + num2
+difference = num1 - num2
 product = num1 * num2
 exponent = num1 ** num2
+
 if num2 != 0:
-    # 4. Division
-    print(" num1 divided by num2 gives:", num1 / num2)
-
-    # 5. Integer Division
-    print("a divided by b for integer division gives:", num1 // num2)
-
-    # 6. Remainder
-    print("the remainder when num1 is divided by num2 is:", num1 % num2)
+    quotient = num1 / num2
+    int_division = num1 // num2
+    remainder = num1 % num2
 else:
     quotient = "undefined (division by zero)"
+    int_division = "undefined"
+    remainder = "undefined"
+
 # Display results
-print(" The sum is ", sum)
-print(" The difference is ", difference)
-print("Th product is :", product)
+print("The sum is:", addition)
+print("The difference is:", difference)
+print("The product is:", product)
 print("The quotient is:", quotient)
-print("The exponentiation result (a^b):", exponent)
+print("The integer division result is:", int_division)
+print("The remainder is:", remainder)
+print("The exponentiation result (a^b) is:", exponent)
